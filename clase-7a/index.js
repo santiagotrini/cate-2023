9515649ed07032dcb083b9785477da08d2e5a3e3
Santiago Trini <santiagotrini@gmail.com>
2023-10-19 21:03:58 -0300

    clase 7 tambien es clase 8

--- clase-7a/index.js
+++ clase-7a/index.js
@@ -1,6 +1,7 @@
 import express from 'express'; // ES6 imports
 import mongoose from 'mongoose';
 import cors from 'cors';
+import morgan from 'morgan';
 import Post from './models/Post.js';
 
 // config vars
@@ -11,29 +12,45 @@ const app = express();
 // agregar middleware
 app.use(express.json());
 app.use(cors());
+app.use(morgan('dev'));
 // connect to DB
 mongoose.connect(DB)
   .then(() => console.log(`DB conectada en ${DB}`))
   .catch(err => console.error(err));
 
 
+//
+// app.get('/', (req, res) => {
+//   res.status(404).json({ msg: 'Not found' });
+// });
+
 app.get('/posts', (req, res) => {
   Post.find()
+    .then(posts => {
+      // console.log(posts);
+      res.status(200).json(posts);
+    })
+    .catch(err => console.error(err));
+});
+// Ejemplo: http://localhost:3000/posts/query?content=Cosas
+app.get('/posts/query', (req, res) => {
+  // console.log(req.query);
+  Post.find(req.query)
     .then(posts => res.status(200).json(posts))
     .catch(err => console.error(err));
 });
 
 app.get('/posts/:id', (req, res) => {
   Post.findById(req.params.id)
-    .then(post => res.status(200).json(post))
-    .catch(err => console.error(err));
+  .then(post => res.status(200).json(post))
+  .catch(err => console.error(err));
 });
 
-app.get('/posts/id/:id', (req, res) => {
-  Post.find({ id: req.params.id })
-    .then(post => res.status(200).json(post))
-    .catch(err => console.error(err));
-});
+// app.get('/posts/id/:id', (req, res) => {
+//   Post.find({ id: req.params.id })
+//     .then(post => res.status(200).json(post))
+//     .catch(err => console.error(err));
+// });
 
 app.post('/posts', (req, res) => {
   console.log(req.body);
@@ -42,11 +59,7 @@ app.post('/posts', (req, res) => {
     .then(post => res.status(201).json(post));
 });
 
-//
-// app.get('/posts/query', (req, res) => {
-//   console.log(req.query);
-//   res.end();
-// });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
--- clase-7b/main.js
+++ clase-7b/main.js
@@ -1,13 +1,50 @@
 let url = 'http://localhost:3000/posts';
 fetch(url)
   .then(res => res.json())
+  // .then(datos => console.log(datos));
   .then(data => {
     loadPosts(data);
   });
 
+function filterByAuthor(event) {
+  event.preventDefault();
+  let author = event.target.author.value;
+  let url = `http://localhost:3000/posts/query?author=${author}`;
+  console.log(url);
+  fetch(url)
+    .then(res => res.json()) // caza el body de la respuesta y hace un JSON.parse
+    .then(data => {
+      loadPosts(data);
+    });
+}
+
+function savePost(event) {
+  event.preventDefault();
+  let author = event.target.author.value;
+  let content = event.target.content.value;
+  let title = event.target.title.value;
+  let id = Math.floor(Math.random() * 15233);
+  let post = { author, content, title, id };
+  let url = 'http://localhost:3000/posts';
+  let options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  };
+  fetch(url,options)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      location.reload();
+    });
+}
+
 function loadPosts(data) {
   let container = document.querySelector('.posts');
-  for (post of data) {
+  container.innerHTML = '';
+  for (let post of data) {
     let postdiv = document.createElement('div');
     let title = document.createElement('h1');
     let author = document.createElement('h3');
